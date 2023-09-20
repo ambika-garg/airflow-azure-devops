0eb4d84a2064804deeb1af9b13553b7c31fa1779
ambika-garg <ambikagarg1101.com>
2023-09-20 11:11:32 -0400

    Max length line in flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@
 # os.environ["AIRFLOW__CORE__LOAD_DEFAULT_CONNECTIONS"] = "False"  # Don't want anything to "magically" work
 # os.environ["AIRFLOW__CORE__LOAD_EXAMPLES"] = "False"  # Don't want anything to "magically" work
 # os.environ["AIRFLOW__CORE__UNIT_TEST_MODE"] = "True"  # Set default test settings, skip certain actions, etc.
-# os.environ["AIRFLOW_HOME"] = os.path.dirname(os.path.dirname(__file__))  # Hardcode AIRFLOW_HOME to root of this project
+# os.environ["AIRFLOW_HOME"] = os.path.dirname(os.path.dirname(__file__))
 
 
 # @pytest.fixture(autouse=True, scope="session")

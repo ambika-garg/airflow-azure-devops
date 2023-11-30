13cf88814a965a3929f03a5e01f93d0045f34c11
Ambika Garg <v-ambikagarg@microsoft.com>
2023-11-30 18:51:16 -0500

    Update  Github actions file

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -5,6 +5,7 @@ import os
 import sys
 import pytest
 from airflow.models import DagBag
+import logging
 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "./dags"))
@@ -15,15 +16,15 @@ def dagbag():
     return DagBag(dag_folder="dags", include_examples=False)
 
 
-# def test_dag(dagbag):
-#     """Validate a complete DAG"""
-#     dagIds = dagbag.dag_ids
-#     # logging.info("dagIds", dagIds)
-#     # print(dagIds)
+def test_dag(dagbag):
+    """Validate a complete DAG"""
+    dagIds = dagbag.dag_ids
+    logging.info("dagIds", dagIds)
+    # print(dagIds)
 
-#     for id in dagIds:
-#         dag = dagbag.get_dag(id)
-#         dag.test()
+    for id in dagIds:
+        dag = dagbag.get_dag(id)
+        dag.test()
 
 
 def test_import_dags(dagbag):

2b711c5e72a1e98c67dd195f9f625fa3d6700b37
Ambika Garg <v-ambikagarg@microsoft.com>
2023-12-01 12:41:13 -0500

    change name of the dag

--- dags/adf.py
+++ dags/adf.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 # A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2023, 8, 15), schedule="0 0 * * *") as dag:
+with DAG(dag_id="demo_one", start_date=datetime(2023, 8, 15), schedule="0 0 * * *") as dag:
 
     # Tasks are represented as operators
     task1 = BashOperator(task_id="hello", bash_command="echo hello")
